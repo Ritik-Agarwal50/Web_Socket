f3795e035f28a22669da00a69804e890d401736c
Ritik <ritikagarwal570@gmail.com>
2024-07-23 15:19:41 +0530

    update

--- manager.go
+++ manager.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
@@ -14,10 +15,15 @@ var (
 	}
 )
 
-type Manager struct{}
+type Manager struct {
+	clients ClientList
+	sync.RWMutex
+}
 
 func NewManager() *Manager {
-	return &Manager{}
+	return &Manager{
+		clients: make(ClientList),
+	}
 }
 func (m *Manager) serveWS(w http.ResponseWriter, r *http.Request) {
 	log.Println("New Connection")
@@ -27,6 +33,22 @@ func (m *Manager) serveWS(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		return
 	}
+	client := NewClient(conn, m)
+	m.addClient(client)
+}
 
-	conn.Close()
+func (m *Manager) addClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+	m.clients[client] = true
+
+}
+
+func (m *Manager) removeClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+	if _,ok :=m.clients[client]; ok {
+		client.connection.Close()
+		delete(m.clients, client)
+	}
 }
